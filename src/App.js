702e05b1b922797c5a15909e1d0b1cdeb5d304fe
RAFH82 <hrechkar@gmail.com>
2020-12-29 20:59:48 -0800

    Starting Chat

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<div className='App'>
-			<Header />
-			<div className='app__body'>
-				<Sidebar />
-			</div>
-			{/* REACT ROUTER */}
+		<div className="app">
+			<Router>
+				<Header />
+				<div className="app__body">
+					<Sidebar />
+					<Switch>
+						<Route path="/room/:roomId">
+							<Chat />
+						</Route>
+						<Route path="/">
+							<Chat />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		</div>
 	);
 }
